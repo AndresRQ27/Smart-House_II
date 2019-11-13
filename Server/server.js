0d39ae47efb33f4dd19ef908c93c187678e9825c
AndresRQ27 <andrew.rq+github@gmail.com>
2019-11-12 19:47:28 -0600

    Bugs & Changes
    Notifications move ot rebounce server
    Image path fixed
    Hex pallete fixed
    Sensor function implemented

--- Server/server.js
+++ Server/server.js
@@ -1,44 +1,41 @@
 /* eslint-disable max-len */
 //  include the http and url module
-const url = require('url');
-const express = require('express');
+const url = require("url");
+const express = require("express");
 const app = express();
-const path = require('path');
-const exec = require('child_process').exec;
-
-// include the module to send notification by auth
-// TODO: descomentar firebase
-// const firebase = require('firebase-admin');
-// const serviceAccount = require('./smarthouseii-firebase-adminsdk-blks5-3986ebe726.json');
+const path = require("path");
+const exec = require("child_process").exec;
+const http = require("http");
 
 // var for the user
-const hostname = '0.0.0.0';
+const hostname = "0.0.0.0";
 const port = 8080;
-// const pathController = 'house_controller';
+const sensorController = "distance"; // Executable for the GPIO
 
 // Login parameters
-const admin = 'admin';
-const adminPassword = '21232f297a57a5a743894a0e4a801fc3'; // MD5('admin')
+const admin = "admin";
+const adminPassword = "21232f297a57a5a743894a0e4a801fc3"; // MD5('admin')
 
 // Toggle parameters
 let alarmBool = false;
 let lockBool = false;
+let sensorLastState = true;
 let lightColor = 0;
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+app.use(function(_req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept',
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   param(req, res);
 });
 
-app.get('/image', (req, res) => {
+app.get("/image", (req, res) => {
   takePhoto(req, res);
 });
 
@@ -64,16 +61,19 @@ function param(req, res) {
   const user = query.user;
   const password = query.password;
 
-  if (typeof user !== 'undefined' && typeof password !== 'undefined') {
+  if (typeof user !== "undefined" && typeof password !== "undefined") {
     authentication(req, res, user, password);
-  } if (typeof alarm !== 'undefined') {
+  }
+  if (typeof alarm !== "undefined") {
     toggleAlarm(req, res);
-  } if (typeof lock !== 'undefined') {
+  }
+  if (typeof lock !== "undefined") {
     toggleLock(req, res);
-  } if (typeof light !== 'undefined') {
+  }
+  if (typeof light !== "undefined") {
     lights(req, res, light);
   } else {
-    console.log('Invalid param');
+    console.log("Invalid param");
     res.sendStatus(400);
   }
 }
@@ -85,16 +85,16 @@ function param(req, res) {
  * @param {*} user: user for the auth
  * @param {*} password: password for the auth
  */
-function authentication(req, res, user, password) {
+function authentication(_req, res, user, password) {
   if (user === admin && password === adminPassword) {
-    console.log('Authenticated');
+    console.log("Authenticated");
     res.json({
-      auth: true,
+      auth: true
     });
   } else {
-    console.log('Not authenticated');
+    console.log("Not authenticated");
     res.json({
-      auth: false,
+      auth: false
     });
   }
 }
@@ -104,12 +104,12 @@ function authentication(req, res, user, password) {
  * @param {*} req: request parameter
  * @param {*} res: response parameter
  */
-function toggleAlarm(req, res) {
+function toggleAlarm(_req, res) {
   alarmBool = !alarmBool;
   console.log(`Alarm on: ${alarmBool}`);
   // TODO: implementar alarma en hardware
   res.json({
-    state: alarmBool,
+    state: alarmBool
   });
 }
 
@@ -118,12 +118,12 @@ function toggleAlarm(req, res) {
  * @param {*} req: request parameter
  * @param {*} res: response parameter
  */
-function toggleLock(req, res) {
+function toggleLock(_req, res) {
   lockBool = !lockBool;
   console.log(`Lock on: ${lockBool}`);
   // TODO: implementar lock en hardware
   res.json({
-    state: lockBool,
+    state: lockBool
   });
 }
 
@@ -133,10 +133,8 @@ function toggleLock(req, res) {
  * @param {*} res: response parameter
  * @param {*} color: number of the color choose
  */
-function lights(req, res, color) {
-  lightColor = color;
-  hexColor = color2Hex(color); // TODO: setear el color en el hardware
-  console.log(`Lights of color: ${hexColor}`);
+function lights(_req, _res, color) {
+  lightColor = color2Hex(color); // TODO: setear el color en el hardware
 }
 
 /**
@@ -144,13 +142,11 @@ function lights(req, res, color) {
  * @param {*} req: request parameter
  * @param {*} res: response parameter
  */
-async function takePhoto(req, res) {
+async function takePhoto(_req, res) {
   // read the image using fs and send the image content back in the response
-  const timestamp = Date.now();
-  const myPhoto = path.resolve(__dirname, `../images/photo.jpeg`);
-  exec('fswebcam -r 640x480 ' + myPhoto,
-      (error, stdout, stderr) => {});
-  console.log('Photo taken');
+  const myPhoto = path.resolve(__dirname, `photo.jpeg`);
+  exec("fswebcam -r 640x480 " + myPhoto, (_error, _stdout, _stderr) => {});
+  console.log("Photo taken");
   await sleep(1500);
   res.sendFile(myPhoto);
 }
@@ -161,7 +157,7 @@ async function takePhoto(req, res) {
  * @return {*} promise of the timeout
  */
 function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 /**
@@ -172,63 +168,86 @@ function sleep(ms) {
 function color2Hex(color) {
   let hex = 0x000000;
   switch (color) {
-    case 0:
+    case "0":
       hex = 0x000000; // TODO: ver como apagar el color
+      console.log("Off");
       break;
-    case 1:
-      hex = 0xFF0000; // Red
+    case "1":
+      hex = 0xff0000; // Red
+      console.log("Red");
       break;
-    case 2:
-      hex = 0x00FF00; // Green
+    case "2":
+      hex = 0x00ff00; // Green
+      console.log("Green");
       break;
-    case 3:
-      hex = 0x0000FF; // Blue
+    case "3":
+      hex = 0x0000ff; // Blue
+      console.log("Blue");
       break;
-    case 4:
-      hex = 0xFFFF00; // Yellow
+    case "4":
+      hex = 0xffff00; // Yellow
+      console.log("Yellow");
       break;
-    case 5:
-      hex = 0xFFA500; // Orange
+    case "5":
+      hex = 0xffa500; // Orange
+      console.log("Orange");
       break;
-    case 6:
+    case "6":
       hex = 0x800080; // Purple
+      console.log("Purple");
       break;
-    case 7:
-      hex = 0xFFC0CB; // Pink
+    case "7":
+      hex = 0xffc0cb; // Pink
+      console.log("Pink");
       break;
-    case 8:
-      hex = 0xFFFFFF; // White
+    case "8":
+      hex = 0xffffff; // White
+      console.log("White");
       break;
-    case 9:
-      hex = 0x00FFFF; // Cyan
+    case "9":
+      hex = 0x00ffff; // Cyan
+      console.log("Cyan");
+      break;
+    default:
+      console.log("Default");
       break;
   }
   return hex;
-};
+}
 
-/*
-// TODO: descomentar el código cuando se usen notificaciones
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-  databaseURL: 'https://smarthouseii.firebaseio.com'
-});
+/**
+ * Function that toggles the param of a room
+ * @param {*} req:
+ * @param {*} res:
+ * @param {*} roomNumber:
+ * @param {*} roomBool:
+ */
+function readSensor() {
+  const readDistance = exec(
+    `${sensorController}`,
+    // eslint-disable-next-line no-unused-vars
+    (_error, _stdout, _stderr) => {}
+  );
+  readDistance.on("exit", code => {
+    if (code === 1 && sensorLastState) {
+      // If state is true, it's a new state
+      console.log("You're too close, GET AWAY FROM ME!");
+      // Get rebounce to send notifications from other server to phone
+      http.get("http://10.23.172.149:9090/", (res) => {}).on("error", (err) => {});
+      sensorLastState = !sensorLastState;
+    } else if (code === 0 && !sensorLastState) {
+      // If state is false, it's a new state
+      console.log("Meh, not close enough");
+      sensorLastState = !sensorLastState;
+    }
+  });
+}
 
-// token const for auth
-const firebaseToken = 'dCkRHXel7-Y:APA91bH-5XebMrTkmm5WU_w8ay1K5PA85h931-s6s-RqLWv9uk24YHjw5MQaksPCWh_4Atdxp5lvGXWpBJDjWCs87jb-rCmmXbzi_MQt80m2f1R5DCaUBWdUp_isEmi2Nj3pcpAwlRHX';
+/**
+ * Function to read the value from the sensor infinitely
+ */
+function run() {
+  setInterval(readSensor, 500); // in mili-seconds
+}
 
-const payload = {
-  notification: {
-    title: 'New guest',
-    body: 'Someone has been detected in your door.'
-  }
-};
-
-const options = {
-  priority: 'high',
-  timeToLive: 60 * 60 * 24 //1 day
-};
-*/
-
-// TODO: implementar la notificación donde corresponde
-// firebase.messaging().sendToDevice(firebaseToken, payload, options);
-// console.log('Notification sent');
+run();
