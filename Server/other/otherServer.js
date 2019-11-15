53eee6d4336c9ef1b24aee020e87d4cefd32b696
AndresRQ27 <andrew.rq+github@gmail.com>
2019-11-15 15:51:25 -0600

    Reorgranized code and notifications implemented
    Lights notification
    Unlock notifications

--- Server/other/otherServer.js
+++ Server/other/otherServer.js
@@ -9,6 +9,39 @@ const serviceAccount = require("./smarthouseii-firebase-adminsdk-k1smw-6286f9e39
 const hostname = "0.0.0.0";
 const port = 9090;
 
+// token const for auth
+const firebaseToken =
+  "e1ZPKhGkMLw:APA91bFmu-eCHFXk23pNbUdG2ocHKpbN9kAxaJOR3e95Js6YbPmqyBw8DdmS-v1X7rGJ1ejZDH-BxNNPLNTSGg8MqHAyy0ewEsdNsEhg-UiRGuxqG10T0phJWKSH6elnl941b1FDwfDI";
+  
+// New guest message
+const newGest = {
+  notification: {
+    title: "New guest",
+    body: "Someone has arrive at your door."
+  }
+};
+
+// Door unlocked message
+const doorUnlocked = {
+  notification: {
+    title: "Door unlocked",
+    body: "Someone has unlocked the door manually."
+  }
+};
+
+// Lights on message
+const lightsOn = {
+  notification: {
+    title: "Lights On",
+    body: "You have left the light on during the day."
+  }
+};
+
+const options = {
+  priority: "high",
+  timeToLive: 60 * 60 * 24 //1 day
+};
+
 app.use(function(_req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -22,14 +55,56 @@ app.get("/", (req, res) => {
   rebounce(req, res);
 });
 
+app.get("/unlock", (req, res) => {
+  unlock(req, res);
+});
+
+app.get("/lights", (req, res) => {
+  lights(req, res);
+});
+
 //  create the http server accepting requests to port 8080
 app.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+/**
+ * Basic functions that notifies when someone is close
+ * @param {*} req: request parameter
+ * @param {*} res: response parameter
+ */
 function rebounce(req, res) {
   console.log("Rebounce");
-  firebase.messaging().sendToDevice(firebaseToken, payload, options);
+  firebase.messaging().sendToDevice(firebaseToken, newGest, options);
+  res.status(200);
+}
+
+/**
+ * Function that notifies if the door is unlocked manually
+ * @param {*} req: request parameter
+ * @param {*} res: response parameter
+ */
+function unlock(req, res) {
+  console.log("Unlock");
+  firebase.messaging().sendToDevice(firebaseToken, doorUnlocked, options);
+  res.status(200);
+}
+
+/**
+ * Function that notifies if the door is unlocked manually
+ * @param {*} req: request parameter
+ * @param {*} res: response parameter
+ */
+function lights(req, res) {
+  let date = new Date();
+  let current_hour = date.getHours();
+
+  if (current_hour > 6 || current_hour < 18) {
+    console.log("Lights during day");
+    firebase.messaging().sendToDevice(firebaseToken, lightsOn, options);
+  } else {
+    console.log("Lights during night");
+  }
   res.status(200);
 }
 
@@ -37,19 +112,3 @@ firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: "https://smarthouseii.firebaseio.com"
 });
-
-// token const for auth
-const firebaseToken =
-  "e1ZPKhGkMLw:APA91bFmu-eCHFXk23pNbUdG2ocHKpbN9kAxaJOR3e95Js6YbPmqyBw8DdmS-v1X7rGJ1ejZDH-BxNNPLNTSGg8MqHAyy0ewEsdNsEhg-UiRGuxqG10T0phJWKSH6elnl941b1FDwfDI";
-
-const payload = {
-  notification: {
-    title: "New guest",
-    body: "Someone has arrive at your door."
-  }
-};
-
-const options = {
-  priority: "high",
-  timeToLive: 60 * 60 * 24 //1 day
-};
